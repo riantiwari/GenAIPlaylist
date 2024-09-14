cc11bb0f0e7bffeb723fd026620a1601ea56aaa7
RianTiwari1 <64282367+riantiwari@users.noreply.github.com>
2024-09-14 18:37:33 -0400

    Revert "Removed Duplicates using Set"
    This reverts commit b9ca74b6096a0d8754c7be0eb9a51aea66328f50.

--- genaiplaylist/flask-server/video_model.py
+++ genaiplaylist/flask-server/video_model.py
@@ -8,14 +8,14 @@ from flask_cors import CORS
 #gets the api_key that I put in the nano /.zshrc file
 #had to do source ~/.zshrc in the vscode terminal as well to make sure this echo'ed properly
 
-app = Flask(__name__)
+app=Flask(__name__)
 CORS(app)
 
 yt_api_key = os.environ.get('YT_API_KEY')
-youtube_service = build('youtube', 'v3', developerKey=yt_api_key)
+youtube_service = build('youtube','v3',developerKey=yt_api_key)
 
-# This method searches YouTube for the parameter search query and returns
-# a link to the video with the highest rating most related to that search
+#this method gets searches youtube for the parameter search query and then returns
+#a link to the video with the highest rating most related to that search
 def get_link(search_name):
     search_request = youtube_service.search().list(
         q=search_name,  # Replace with your search query
@@ -34,18 +34,21 @@ def get_link(search_name):
 
     best_video = max(video_response['items'], key=lambda x: int(x['statistics'].get('likeCount', 0)))
     best_video_id = best_video['id']
+    #best_video_title = best_video['snippet']['title']
     video_link = f"https://www.youtube.com/watch?v={best_video_id}"
     return video_link
 
 @app.route("/home", methods=['GET'])
 def playlist():
-    playlist_links = set()  # Using a set to avoid duplicate links
-    # Generate the playlist links by calling get_link for each item from llm_response
+    playlist_links = list()
+    #comment this stuff out for now because I exceeded my quota limit
     for i in llm_response.lst:
-        playlist_links.add(get_link(i))  # add() ensures no duplicates
+        playlist_links.append(get_link(i))
+    # playlist_links = [
+    #     "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    #     "https://www.youtube.com/watch?v=DjYZk8nrXVY"
+    # ]
+    return jsonify({"home": playlist_links})
 
-    # Convert set to list for JSON serialization
-    return jsonify({"home": list(playlist_links)})
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
